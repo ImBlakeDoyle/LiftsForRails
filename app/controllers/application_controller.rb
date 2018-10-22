a35eb8b5848246018e6b77973e28ad3f834616b6
Blake <blake.doyle@hotmail.com.au>
2018-10-23 09:39:25 +1100

    get the new registration parameters to save

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+before_action :add_devise_params, if: :devise_controller?
+
+    def add_devise_params
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :image])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:name, :image])
+    end
 end
